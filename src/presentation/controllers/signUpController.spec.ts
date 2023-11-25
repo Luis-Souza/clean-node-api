d139d939d60e747746dc720d938791ffbc9a969b
Jorge Luis <jorgeluis0796@gmail.com>
2023-11-24 22:09:03 -0300

    refactor: define types to params and returns

--- src/presentation/controllers/signUpController.spec.ts
+++ src/presentation/controllers/signUpController.spec.ts
@@ -1,31 +1,32 @@
+import { type HttpRequest, type HttpResponse } from '../protocols/http'
 import { SignUpController } from './signUpController'
 
 describe('SignUp Controller', () => {
   it('Should be return 400 if no name is provided', () => {
     const sut = new SignUpController()
-    const httpRequest = {
+    const httpRequest: HttpRequest = {
       body: {
         email: 'teste@gmail.com',
         password: '231',
         passwordConfirmation: '231'
       }
     }
-    const httpResponse = sut.handler(httpRequest)
-    expect(httpResponse.statusCode).toBe(400)
-    expect(httpResponse.body).toEqual(new Error('Missing param: name'))
+    const httpResponse: HttpResponse | undefined = sut.handler(httpRequest)
+    expect(httpResponse?.statusCode).toBe(400)
+    expect(httpResponse?.body).toEqual(new Error('Missing param: name'))
   })
 
   it('Should be return 400 if no email is provided', () => {
     const sut = new SignUpController()
-    const httpRequest = {
+    const httpRequest: HttpRequest = {
       body: {
         name: 'any_name',
         password: '231',
         passwordConfirmation: '231'
       }
     }
-    const httpResponse = sut.handler(httpRequest)
-    expect(httpResponse.statusCode).toBe(400)
-    expect(httpResponse.body).toEqual(new Error('Missing param: email'))
+    const httpResponse: HttpResponse | undefined = sut.handler(httpRequest)
+    expect(httpResponse?.statusCode).toBe(400)
+    expect(httpResponse?.body).toEqual(new Error('Missing param: email'))
   })
 })
